028203f676b6e7dcabacad7ca87857aa049a9d19
jonathanheilmann <mail@jonathan-heilmann.de>
2015-08-15 13:57:14 +0200

    [TASK] Upload version 0.1.0-rc1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,40 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jh_smilie_viewhelper".
-#
-# Auto generated 15-08-2015 13:50
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_smilie_viewhelper".
+ *
+ * Auto generated 10-02-2015 18:03
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Smilie Viewhelper',
 	'description' => 'This extensions provides two fluid viewhelpers for EXT:smilie',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '0.0.3',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '4.5.0-6.1.99',
-	'PHP_version' => '',
-	'module' => '',
+	'version' => '0.1.0-rc1',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '4.5.0-6.2.99',
+			'smilie' => '',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
 );
 
-?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Add static files
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Javascript', 'Smilies by Javascript');
+
+
 ?>
